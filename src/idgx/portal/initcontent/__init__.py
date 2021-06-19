d0bf0c551d1d7ade5c5b116ad12a7a04728dbb2c
Cleber J Santos <cleber@cleberjsantos.com.br>
2021-06-19 15:14:43 -0300

    Fix contents

--- src/idgx/portal/initcontent/__init__.py
+++ src/idgx/portal/initcontent/__init__.py
@@ -8,13 +8,13 @@ from plone.app.dexterity.behaviors import constrains
 from plone.app.textfield.value import RichTextValue
 from plone.dexterity.utils import addContentToContainer
 from plone.dexterity.utils import createContent
-from plone.dexterity.utils import createContentInContainer
 from plone.i18n.normalizer import idnormalizer
 from plone.locking.interfaces import ILockable
 from plone.namedfile.file import NamedBlobImage
 from plone.registry.interfaces import IRegistry
 from zope.component import queryUtility
 from zope.i18n.interfaces import ITranslationDomain
+from zope.i18n.locales import LoadLocaleError
 from zope.i18n.locales import locales
 
 import base64
@@ -71,14 +71,14 @@ def _get_locales_info(portal):
 def cleaning_house(portal):
     """ Removing default objects of Plone """
     rm_ids = ['front-page', 'news', 'events', 'noticias', 'eventos', 'home',
-            'pagina-inicial']
+              'pagina-inicial']
     remove_list = [obj_id for obj_id in rm_ids if obj_id in portal.keys()]
 
     if len(remove_list) > 0:
         for obj_id in remove_list:
             obj = portal[obj_id]
 
-            if not 'Folder' in obj.portal_type:
+            if 'Folder' not in obj.portal_type:
                 lockable = ILockable(portal[obj_id])
 
                 if lockable.locked():
@@ -88,10 +88,7 @@ def cleaning_house(portal):
 
 def create_topic(portal, target_language):
     news_id = idnormalizer.normalize(_translate(u'news-title', target_language,
-        u'News'))
-
-    #images_id = idnormalizer.normalize(_translate(u'images-title', target_language,
-    #    u'Images'))
+                                     u'News'))
 
     if news_id not in portal.keys():
         title = _translate(u'news-title', target_language, u'News')
@@ -105,7 +102,7 @@ def create_topic(portal, target_language):
         container = addContentToContainer(portal, container)
 
         collection_id = idnormalizer.normalize(_translate(u'list-title',
-            target_language, u'List'))
+                                               target_language, u'List'))
         _createObjectByType('Collection', container,
                             id=collection_id, title=title,
                             description=description)
@@ -138,87 +135,46 @@ def create_topic(portal, target_language):
         _publish(aggregator)
 
 
-    #if images_id not in portal.keys():
-    #    title = _translate(u'images-title', target_language, u'Images')
-    #    description = _translate(u'images-description', target_language,
-    #                             u'Images folder')
-    #    container = createContent(
-    #        'Folder', id=images_id,
-    #        title=title,
-    #        description=description,
-    #        language=target_language.replace('_', '-').lower())
-    #    container = addContentToContainer(portal, container)
-
-    #    imgs_collection_id = idnormalizer.normalize(_translate(u'list-title',
-    #        target_language, u'List'))
-    #    _createObjectByType('Collection', container,
-    #                        id=imgs_collection_id, title=title,
-    #                        description=description)
-    #    aggregator = container[imgs_collection_id]
-
-    #    # Constrain types
-    #    allowed_types = ['Image', ]
-    #    _setup_constrains(container, allowed_types)
-
-    #    container.setOrdering('unordered')
-    #    container.setDefaultPage(imgs_collection_id)
-    #    _publish(container)
-
-    #    # Set the Collection criteria.
-    #    #: Sort on the Effective date
-    #    aggregator.sort_on = u'effective'
-    #    aggregator.sort_reversed = True
-    #    #: Query by Type and Review State
-    #    aggregator.query = [
-    #        {'i': u'portal_type',
-    #         'o': u'plone.app.querystring.operation.selection.any',
-    #         'v': [u'Image'],
-    #         },
-    #    ]
-    #    aggregator.setLayout('tabular_view')
-    #    _publish(aggregator)
-
-
 def read_directory(directory):
-        data_file = 'data.json'
-        children_file = 'children.json'
+    data_file = 'data.json'
+    children_file = 'children.json'
 
-        path = '{0}/{1}'.format(directory, data_file)
-        try:
-            data = json.loads(open(path, 'r').read())
-        except IOError:
-            # Arquivo data.json não existe
-            data = {}
-        except ValueError:
-            # json mal formado
-            data = {}
+    path = '{0}/{1}'.format(directory, data_file)
+    try:
+        data = json.loads(open(path, 'r').read())
+    except IOError:
+        # Arquivo data.json não existe
+        data = {}
+    except ValueError:
+        # json mal formado
+        data = {}
 
-        yield data
+    yield data
 
-        path = '{0}/{1}'.format(directory, children_file)
-        try:
-            children = json.loads(open(path, 'r').read())
-        except IOError:
-            # Arquivo children.json não existe.
-            children = []
-        except ValueError:
-            # json mal formado
-            children = []
-        for child in children:
-            oId = child.get('id')
-            path = '%s/%s' % (directory, oId)
-            for item in read_directory(path):
-                yield item
+    path = '{0}/{1}'.format(directory, children_file)
+    try:
+        children = json.loads(open(path, 'r').read())
+    except IOError:
+        # Arquivo children.json não existe.
+        children = []
+    except ValueError:
+        # json mal formado
+        children = []
+    for child in children:
+        oId = child.get('id')
+        path = '%s/%s' % (directory, oId)
+        for item in read_directory(path):
+            yield item
 
 
 def datetime_to_integer(dt_time):
     dt = datetime.strptime(dt_time, '%Y-%m-%d %H:%M:%S')
-    return 10000*dt.year + 100*dt.month + dt.day
+    return 10000 * dt.year + 100 * dt.month + dt.day
 
 
 def dummy_image(filename=u'image.jpg', b64=None):
     if b64:
-        im = BytesIO(base64.b64decode( re.sub("data:image/jpeg;base64", '', b64) ))
+        im = BytesIO(base64.b64decode(re.sub("data:image/jpeg;base64", '', b64)))
         image_data = im.read()
     else:
         filename = os.path.join(os.path.dirname(__file__), filename)
@@ -235,7 +191,7 @@ def create_newsitem(portal, target_language):
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     news_id = idnormalizer.normalize(_translate(u'news-title', target_language,
-        u'News'))
+                                     u'News'))
     path_to_json = glob.glob('{}/json/**'.format(dir_path))
 
     if news_id in portal.keys():
@@ -281,7 +237,7 @@ def create_images(portal, target_language):
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     images_id = idnormalizer.normalize(_translate(u'images-title', target_language,
-        u'Images'))
+                                       u'Images'))
     path_to_json = glob.glob('{}/json/**'.format(dir_path))
 
     if images_id in portal.keys():
@@ -290,27 +246,24 @@ def create_images(portal, target_language):
         for jpath in path_to_json:
             for item in read_directory(jpath):
 
-                    if item['_type'] == 'Image':
-                        img_data = item['_datafield_image']
-                        content = createContent(
-                            item['_type'], id=item['_id'],
-                            language=target_language.replace('_', '-').lower(), **item)
-                        #pth = os.path.basename(item['_path'].strip('{}'.format(item['_id'])))
-                        pth = item['_path'].rstrip('{}'.format(item['_id']))
-                        content = addContentToContainer(container.unrestrictedTraverse(pth), content)
+                if item['_type'] == 'Image':
+                    img_data = item['_datafield_image']
+                    content = createContent(
+                        item['_type'], id=item['_id'],
+                        language=target_language.replace('_', '-').lower(), **item)
+                    pth = item['_path'].rstrip('{}'.format(item['_id']))
+                    content = addContentToContainer(container.unrestrictedTraverse(pth), content)
 
-                        content.image = dummy_image(filename=img_data['filename'], b64=img_data['data'])
-                        content.reindexObject()
+                    content.image = dummy_image(filename=img_data['filename'], b64=img_data['data'])
+                    content.reindexObject()
 
 
 def create_cover(portal, target_language):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     news_id = idnormalizer.normalize(_translate(u'news-title', target_language,
-        u'News'))
-    collection_id = idnormalizer.normalize(_translate(u'list-title',
-            target_language, u'List'))
+                                     u'News'))
     cover_id = idnormalizer.normalize(_translate(u'initial-page', target_language,
-        u'Home'))
+                                      u'Home'))
     path_to_json = glob.glob('{}/json/**'.format(dir_path))
 
     if cover_id not in portal.keys():
@@ -334,7 +287,7 @@ def create_cover(portal, target_language):
                                 {'tile': 'bef3a69c-df38-4763-80df-ec57f03db3d1', 'content': 'noticia-de-exemplo-1'},
                                 {'tile': 'facfd146-f324-4cc5-bbfc-9d8113947291', 'content': 'noticia-de-exemplo-5'},
                                 {'tile': 'fa84c576-0795-49c8-a596-5cbd2260d210', 'content': 'noticia-de-exemplo-3'},
-                                {'tile': 'c236aeb0-d651-4f99-b325-4cb5d1bf5d74', 'content': 'noticia-de-exemplo-4',}]
+                                {'tile': 'c236aeb0-d651-4f99-b325-4cb5d1bf5d74', 'content': 'noticia-de-exemplo-4'}]
 
                     for tile_content in contents:
                         content.get_tile(tile_content['tile']).populate_with_object(content_container[tile_content['content']])
@@ -344,7 +297,6 @@ def create_cover(portal, target_language):
         if not hasattr(portal, 'default_page'):
             portal.manage_addProperty('default_page',
                                       cover_id, 'string')
-
 
 
 def import_content(context):
